334ecb3e97ccd34cdfcf483fcd9544393028915c
Alex <alex.chart@turningpointhr.com>
2021-12-15 14:26:05 +0000

    commit tree b44fc4b6b9e673d1ecd655913e8dca7e39e775a1

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
-
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -24,15 +23,63 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
-
 // Strings
-
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
+const animalSounds = ['cow', 'horse', 'sheep', 'pig', 'chicken']
+const [moo, neigh, baa, oink, cluck] = animalSounds;
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
+const animals = {
+  bessie: "cow",
+  dolly: "sheep",
+  babe: "pig",
+  little: "chicken"
+}
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
+const {bessie, dolly, babe, little} = animals
+const diffColors = {
+  blackAndWhite: "cow",
+  black: "sheep",
+  pink: "pig"
+}
+const { blackAndWhite, black, pink } = diffColors
+const colorRainbow = {
+  red: "red",
+  orange: "orange",
+  yellow: "yellow",
+  green: "green",
+  blue: "blue",
+  indigo: "indigo",
+  violet: "violet"
+}
+const { red, orange, yellow, green, blue, indigo, violet } = colorRainbow
+const rainbow = {
+  r: "red",
+  o: "orange",
+  y: "yellow",
+  g: "green",
+  b: "blue",
+  v: "violet"
+}
+const { r, o, y, g, b, v } = rainbow
+const diffPurp = {
+  indg: "indigo"
+}
+const { indg } = diffPurp
+const objectDecon = {
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit"
+}
+const { muppetName, color, song, job, partner } = objectDecon
+const songsToSing = {
+  song2: "Moving Right Along",
+  song4: "I Hope That Something Better Comes Along",
+  nestedJob: "Host of The Muppet Show",
+  nestedPartner: "Miss Piggy"
+}
+const { song2, song4, nestedJob, nestedPartner } = songsToSing
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
